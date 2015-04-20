24ad0d4132dd9d6d8408c124b4b960e3376440b5
kimjune01 <kimjune01@gmail.com>
2015-04-19 19:39:32 -0700

    added gitignore and pod

---
+++ MediaComponent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MediaComponent
+//
+//  Created by Camvy Films on 2015-04-19.
+//  Copyright (c) 2015 June. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
